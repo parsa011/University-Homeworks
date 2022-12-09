57a8146b0e8e8edb0a57f9e5aa465c23807f4830
parsa mahmoudy <43126637+parsa011@users.noreply.github.com>
2022-12-09 11:20:07 +0330

    writing all of 17 Azar

--- Term 1/17 Azar/7-primeNumbers.cpp
+++ Term 1/17 Azar/7-primeNumbers.cpp
@@ -13,8 +13,6 @@ int main()
   min_n = min(n, m);
   int prime_numbers[max_n];
   int i;
-  for (i = 0; i < min_n; i++)
-    prime_numbers[i] = 0;
   for (i = min_n; i < max_n; i++)
     prime_numbers[i] = 1;
   for (i = 2; i <= max_n / 2; i++) {
