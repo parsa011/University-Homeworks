ab74b928c683ea1fe1e30fc45c0d4b26b864c6f9
parsa011 <parsa32569@gmail.com>
2023-01-07 22:55:36 +0330

    big int sub is done :)

---
+++ Term 1/40 Soal/6.cpp
@@ -0,0 +1,183 @@
+#include "global.h"
+
+typedef struct {
+	int len;
+	int *val;
+	bool negative;
+} bigint;
+
+#define INIT_BIGINT {0, NULL, false}
+#define BIGINT_SIZE 10
+
+void bigint_sum(bigint *, bigint *);
+void bigint_sub(bigint *, bigint *);
+void get_bigint(bigint *);
+void print_bigint(bigint buf);
+
+void bigint_print(bigint buf)
+{
+	if (buf.negative)
+		cout << '-';
+	for (int i = 0; i < buf.len; i++) {
+		cout << buf.val[i];
+	}
+	cout << endl;
+}
+
+/*
+ gets a big int until reaches to max length and return length of given number
+*/
+void get_bigint(bigint *buf)
+{
+	char ch;
+	int bufsize = BIGINT_SIZE;
+	buf->len = 0;
+	buf->val = (int *)malloc(bufsize * sizeof(int));
+	do {
+		ch = getchar();
+		if (ch == '\n')
+			break;
+		else if (ch == 0x7F) {
+			if (buf->len > 0) {
+				buf->val[--buf->len] = 0;
+			}
+		}
+
+		if (buf->len == bufsize) {
+			bufsize += BIGINT_SIZE; 
+			buf->val = (int *)realloc(buf->val, bufsize * sizeof(int));
+		}
+		if (isdigit(ch) || ch == '-') {
+			if (ch == '-') {
+				if (buf->len == 0) 
+					buf->negative = true;
+				else {
+					cout << "invalid number\n";
+					exit(0);
+				}
+			} else {
+				buf->val[buf->len] = ch - '0';
+				buf->len++;
+			}
+		} else {
+			cout << "Enter Valid Number\n";
+			exit(0);
+		}
+	} while(true);
+}
+
+int bigint_bigger(bigint *a, bigint *b)
+{
+#define RIGHT 1
+#define LEFT -1
+#define EQUAL 0
+	if (!a->negative && b->negative)
+		return LEFT;
+	if (a->negative && !b->negative)
+		return RIGHT;
+	if (a->len == b->len) {
+		for (int i = 0; i < a->len; i++) {
+			if (a->val[i] > b->val[i])
+				return LEFT;
+			else if (a->val[i] < b->val[i])
+				return RIGHT;
+			else
+				continue;
+		}
+		return EQUAL;
+	} else {
+		if (a->len > b->len)
+			return LEFT;
+		if (a->len < b->len)
+			return RIGHT;
+	}
+	return EQUAL;
+#undef RIGHT
+#undef LEFT
+#undef EQUAL
+}
+
+void bigint_sub(bigint *a, bigint *b)
+{
+	if (!a->negative && !b->negative) {
+		bigint_sum(a, b);
+		return;
+	}
+	bigint *bigger;
+	bigint *smaller;
+	/*
+	  here we stored b sign , because we need to check which number is bigger
+	  set a and b sign to positive , then check , and after check restore b sign
+	  a sign will be bigger number sign
+	*/
+	bool b_sign = b->negative;
+	a->negative = b->negative = false;
+	int status = bigint_bigger(a, b);
+	b->negative = b_sign;
+	
+	bigger = status == 1 ? b : a;
+	smaller = status == -1 ? b : a;
+	bool dec = false;
+	string res = "";
+	int i = bigger->len - 1, j = smaller->len - 1, temp;
+	for (; i >= 0 || j >= 0;) {
+		int n1 = bigger->val[i--];
+		int n2 = j >= 0 ? smaller->val[j--] : 0;
+		if (dec)
+			n1--;
+		if (n2 > n1) {
+			dec = true;
+			n1 += 10;
+		}
+		temp = abs(n2 - n1);
+		res += to_string(temp);
+	}
+	reverse_string(res);
+	a->len = res.length();
+	free(a->val);
+	a->negative = bigger->negative;
+	a->val = (int *)malloc(a->len * sizeof(int));
+	for (i = 0; i < res.length(); i++) {
+		a->val[i] = res[i] - '0';
+	}
+}
+
+void bigint_sum(bigint *a, bigint *b)
+{
+	if (a->negative != b->negative) {
+		bigint_sub(a, b);
+		return;
+	}
+	int new_len = max(a->len, b->len);
+	int buf[new_len];
+	int k = new_len - 1, temp;
+	int i = a->len - 1, j = b->len - 1;
+	int carry;
+	for (; i >= 0 || j >= 0;) {
+		int n1 = i >= 0 ? a->val[i--] : 0;
+		int n2 = j >= 0 ? b->val[j--] : 0;
+		temp = n1 + n2 + carry;
+		carry = temp / 10;
+		buf[k--] = temp % 10;
+	}
+	new_len = carry ? new_len + 1 : new_len;
+	free(a->val);
+	a->len = new_len;
+	a->val = (int *)malloc(new_len * sizeof(int));
+	i = j = 0;
+	if (carry) {
+		a->val[i++] = 1;
+	}
+	for (; i < new_len; i++)
+		a->val[i] = buf[j++];
+}
+
+int main()
+{
+	bigint a = INIT_BIGINT, b = INIT_BIGINT;
+	get_bigint(&a);
+	get_bigint(&b);
+	bigint_sum(&a, &b);
+	bigint_print(a);
+	return 0;
+}
--- Term 1/40 Soal/global.h
+++ Term 1/40 Soal/global.h
@@ -8,7 +8,9 @@
 #include <string.h>
 #include <fstream>
 #include <math.h>
+#if defined(_WIN32)
 #include <Windows.h>
+#endif
 
 /*
   it can be a little dangerous , but not in our project i think :)))
@@ -17,82 +19,95 @@ using namespace std;
 
 static bool file_exist(char *file_path)
 {
-  fstream file(file_path);
-  bool is_open = file.is_open();
-  file.close();
-  return is_open;
+	fstream file(file_path);
+	bool is_open = file.is_open();
+	file.close();
+	return is_open;
 }
 
 static long file_line_count(fstream &file)
 {
-  long count = 0;
-  string line;
-  while (getline(file, line))
-    count++;
-  // move cursor to the head of file
-  file.clear();
-  file.seekg(0);
-  return count;
+	long count = 0;
+	string line;
+	while (getline(file, line))
+		count++;
+	// move cursor to the head of file
+	file.clear();
+	file.seekg(0);
+	return count;
 }
 
 static void eat_until(string str, char ch, int &i)
 {
-  i = str.find(ch) + 1;
+	i = str.find(ch) + 1;
 }
 
+/*
+  also we can do this with a pointer
+*/
 static void skip_space(string str, int &i)
 {
-  while (str[i]) {
-    if (!isspace(str[i]))
-      break;
-    i++;
-  }
+	while (str[i]) {
+		if (!isspace(str[i]))
+			break;
+		i++;
+	}
 }
 
 static int lex_int(string str, int &i)
 {
-  skip_space(str, i);
-  int sign = 1;
-  if (str[i] == '-') {
-    sign = -1;
-    i++;
-  }
-  int res = 0;
-  while (isdigit(str[i])) {
-    res = res * 10 + (str[i++] - '0');
-  }
-  return res * sign;
+	skip_space(str, i);
+	int sign = 1;
+	if (str[i] == '-') {
+		sign = -1;
+		i++;
+	}
+	int res = 0;
+	while (isdigit(str[i])) {
+		res = res * 10 + (str[i++] - '0');
+	}
+	return res * sign;
 }
 
 static string lex_string(string str, int &i)
 {
-  skip_space(str, i);
-  string s;
-  while (str[i]) {
-    if (isspace(str.at(i)))
-	break;
-    s += str[i++];
-  }
-  return s;
+	skip_space(str, i);
+	string s;
+	while (str[i]) {
+		if (isspace(str.at(i)))
+			break;
+		s += str[i++];
+	}
+	return s;
 }
 
 static int decimal_len(long n)
 {
-  n = abs(n);
-  int len = 1;
-  while (n / 10) {
-    n /= 10;
-    len++;
-  }
-  return len;
+	n = abs(n);
+	int len = 1;
+	while (n / 10) {
+		n /= 10;
+		len++;
+	}
+	return len;
 }
 
 static int nthdigit(int n, int x)
 {
-  while (--x) {
-    n /= 10;
-  }
-  return n % 10;
+	while (--x) {
+		n /= 10;
+	}
+	return n % 10;
+}
+
+static void reverse_string(string &str)
+{
+	char ch;
+	for (int index = 0, len = str.length(); index < len / 2; index++) {
+      ch = str[index];
+      str[index] = str[len-1-index];
+      str[len-1-index] = ch;
+	}
 }
 
 
