88b25309013a22dd94cc8d86da83e77a6f5bdbc8
parsa mahmoudy <43126637+parsa011@users.noreply.github.com>
2023-01-13 00:37:27 +0330

    working on promblem no 26

--- Term 1/40 Soal/26.cpp
+++ Term 1/40 Soal/26.cpp
@@ -12,17 +12,8 @@ void get_collection(char db[], int &len)
 	}
 }
 
-void create_possible_combinations(string db[], int index, string bag, int max_len)
+void create_possible_combinations(char db[], int len, int index, string str)
 {
-	if (bag.length() > max_len)
-		return;
-	bag += "0";
-	db[index++] = bag;
-	create_possible_combinations(db, index, bag, max_len);
-	string temp(bag);
-	bag += "1";
-	db[index++] = temp;
-	create_possible_combinations(db, index, temp, max_len);
 }
 
 int main()
@@ -35,10 +26,8 @@ int main()
 	} while (len > MAX);
 	get_collection(db, len);
 	int sub_collections = (int)pow(2, len) - 1;
-	string combinations[sub_collections];
-	create_possible_combinations(combinations, 0, "", len);
-	for (int i = 0; i < sub_collections; i++) {
-		cout << combinations[i] << endl;
+	for (int i = 0; i < len; i++) {
+		create_possible_combinations(db, len, db[i], i);
 	}
 	return 0;
 }
